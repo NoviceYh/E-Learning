508fb45fb2d7ee1e487c948127b6eaa642b9c3eb
Alejandro Aguirre <aguirreee21@gmail.com>
2023-11-29 12:41:18 -0300

    Creating E-Learning Proyect

---
+++ src/main/java/com/codigozero/elearningplatform/PlataformaELearningApplication.java
@@ -0,0 +1,13 @@
+package com.codigozero.elearningplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlataformaELearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlataformaELearningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codigozero/elearningplatform/PlataformaELearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codigozero.elearningplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlataformaELearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
